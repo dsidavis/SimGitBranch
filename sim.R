143af3cbf051dc15a226723c269610223ee08893
Duncan Temple Lang <duncan@r-project.org>
2019-05-31 09:49:44 -0700

    extended the function to allow parameterizing n and rate

--- sim.R
+++ sim.R
@@ -1,11 +1,12 @@
 sim =
-function(T = max, B = 999)
+function(T = max, B = 999, n = 35, ...)
 {
-   replicate(B, T(rexp(35, 1)))
+   replicate(B, T(rexp(n, ...)))
 }
 
 ans = sim()
 ans = sim(min)
+ans = sim(function(x) c(min(x), max(x)))
 
 
 
